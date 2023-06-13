52a1dc691e64352ac5d076d537ec4dc3f604680c
g-kimani <77020194+g-kimani@users.noreply.github.com>
2023-06-13 16:20:22 +0100

    added login and wip signup

--- App.tsx
+++ App.tsx
@@ -1,36 +1,50 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import {
+  createNativeStackNavigator,
+  NativeStackScreenProps,
+} from "@react-navigation/native-stack";
 import HomePage from "./screens/HomePage";
 import SignupScreen from "./screens/SignupScreen";
-import LogIn from "./screens/Login";
+import LoginScreen from "./screens/LoginScreen";
 import Map from "./screens/Map";
-
-type NavigationStackParamList = {
-  // provide the props that are needed for a route, undefined if no props
-  Login: undefined;
-
-  Signup: undefined;
-  HomePage: undefined;
-  Map: undefined;
-  // example of props and types | undefined means props optional
-  Profile: { userid: "string" } | undefined;
-};
+import { UserContext } from "./contexts/UserContext";
+import { useState, useEffect } from "react";
+import * as SecureStore from "expo-secure-store";
 
 const Stack = createNativeStackNavigator<NavigationStackParamList>();
 
+type NavigationStackParamList = {
+  Map: undefined;
+  HomePage: undefined;
+  Login: undefined;
+  Signup: undefined;
+  Profile: { userid: "string" } | undefined;
+  navigate: {};
+};
 export default function App() {
+  const [user, setUser] = useState({});
+  useEffect(() => {
+    SecureStore.getItemAsync("auth-token").then((result) => {
+      if (result) {
+        setUser((user) => {
+          return { ...user, token: result };
+        });
+      }
+    });
+  }, []);
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={LogIn} />
-
-        <Stack.Screen name="Signup" component={SignupScreen} />
-        <Stack.Screen name="HomePage" component={HomePage} />
-        <Stack.Screen name="Map" component={Map} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <UserContext.Provider value={{ user, setUser }}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Signup" component={SignupScreen} />
+          <Stack.Screen name="HomePage" component={HomePage} />
+          <Stack.Screen name="Map" component={Map} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </UserContext.Provider>
   );
 }
 
