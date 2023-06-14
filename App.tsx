8ae1b20ce9cb5944d8befb15f7c456647499e5f9
Northerner988 <lulu12100@hotmail.co.uk>
2023-06-14 22:14:21 +0100

    fix formating, remove unnecessary import statements and comments

--- App.tsx
+++ App.tsx
@@ -1,21 +1,13 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import {
   createNativeStackNavigator,
   NativeStackScreenProps,
 } from "@react-navigation/native-stack";
-import HomePage from "./screens/HomePage";
 import SignupScreen from "./screens/SignupScreen";
 import LoginScreen from "./screens/LoginScreen";
-import Profile from "./screens/Profile";
-import MapView from "./Components/MapView";
 import { UserContext } from "./contexts/UserContext";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import * as SecureStore from "expo-secure-store";
-import AddParkingList from "./screens/AddParkingList";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
 import HomeTabNav from "./Navigation/HomeTabNav";
 
 const Stack = createNativeStackNavigator();
@@ -27,34 +19,6 @@ export type NavigationStackParamList = {
   Profile: { userid: string } | undefined;
   AddParking: undefined;
 };
-
-//   return (
-//     <UserContext.Provider value={{ user, setUser }}>
-//       <NavigationContainer>
-//         {!user.token ? (
-//           <Stack.Navigator>
-//             <Stack.Screen
-//               name="Login"
-//               component={LoginScreen}
-//               options={{ title: "Sign In" }}
-//             />
-//             <Stack.Screen
-//               name="Signup"
-//               component={SignupScreen}
-//               options={{ title: "Create Account" }}
-//             />
-//           </Stack.Navigator>
-//         ) : (
-//           <Tab.Navigator>
-//             <Tab.Screen name="Home" component={HomeStackScreen} />
-//             <Tab.Screen name="Profile" component={ProfileStackScreen} />
-//             <Tab.Screen name="AddParking" component={AddParkingStackScreen} />
-//           </Tab.Navigator>
-//         )}
-//       </NavigationContainer>
-//     </UserContext.Provider>
-//   );
-// }
 
 export default function App() {
   const [user, setUser] = useState({});
@@ -71,30 +35,32 @@ export default function App() {
   return (
     <UserContext.Provider value={{ user, setUser }}>
       <NavigationContainer>
-        {!user.token ? (
-          <Stack.Navigator>
-            <Stack.Screen
-              name="Login"
-              component={LoginScreen}
-              options={{ title: "Sign In" }}
-            />
-            <Stack.Screen
-              name="Signup"
-              component={SignupScreen}
-              options={{ title: "Create Account" }}
-            />
-          </Stack.Navigator>
-        ) : (
-          <Stack.Navigator>
-            <Stack.Screen
-              name="Home"
-              component={HomeTabNav}
-              options={{
-                headerShown: false,
-              }}
-            />
-          </Stack.Navigator>
-        )}
+        <Stack.Navigator>
+          {!user.token ? (
+            <>
+              <Stack.Screen
+                name="Login"
+                component={LoginScreen}
+                options={{ title: "Sign In" }}
+              />
+              <Stack.Screen
+                name="Signup"
+                component={SignupScreen}
+                options={{ title: "Create Account" }}
+              />
+            </>
+          ) : (
+            <>
+              <Stack.Screen
+                name="Home"
+                component={HomeTabNav}
+                options={{
+                  headerShown: false,
+                }}
+              />
+            </>
+          )}
+        </Stack.Navigator>
       </NavigationContainer>
     </UserContext.Provider>
   );
