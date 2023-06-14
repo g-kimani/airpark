d2957a5b10bb37d5053bac103a54f5e384f3b8bb
Northerner988 <lulu12100@hotmail.co.uk>
2023-06-14 21:44:59 +0100

    remove unnecessary import statements

--- Navigation/HomeTabNav.tsx
+++ Navigation/HomeTabNav.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { View, Text } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomePage from "../screens/HomePage";
 import { Ionicons, AntDesign } from "@expo/vector-icons";
