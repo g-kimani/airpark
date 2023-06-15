4ddc031a7ea5c73cac897d2926c3be1da3a8ee57
Northerner988 <lulu12100@hotmail.co.uk>
2023-06-15 13:54:06 +0100

    fix props issue

--- Components/MapView.tsx
+++ Components/MapView.tsx
@@ -1,8 +1,6 @@
-import MapView from "react-native-maps";
-import { Marker } from "react-native-maps";
-import { FlatList, StyleSheet } from "react-native";
-import { Text } from "react-native";
-import * as Location from "expo-location";
+import React from "react";
+import { StyleSheet } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 
 type Props = {
   selectedLocation: {
@@ -14,38 +12,37 @@ type Props = {
 
 const MapComponent = ({ selectedLocation, parkings }: Props) => {
   return (
-    <>
-      <MapView
-        style={styles.map}
-        region={{
-          latitude: selectedLocation ? selectedLocation.latitude : 0,
-          longitude: selectedLocation ? selectedLocation.longitude : 0,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      >
-        <Marker coordinate={{ latitude: 51.509865, longitude: -0.118092 }} />
+    <MapView
+      style={styles.map}
+      region={{
+        latitude: selectedLocation ? selectedLocation.latitude : 0,
+        longitude: selectedLocation ? selectedLocation.longitude : 0,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+    >
+      <Marker coordinate={{ latitude: 51.509865, longitude: -0.118092 }} />
 
-        {parkings.map((parking) => {
-          return (
-            <Marker
-              key={parking.parking_id}
-              coordinate={{
-                latitude: parking.latitude,
-                longitude: parking.longitude,
-              }}
-            />
-          );
-        })}
-      </MapView>
-    </>
+      {parkings.map((parking) => {
+        return (
+          <Marker
+            key={parking.parking_id}
+            coordinate={{
+              latitude: parking.latitude,
+              longitude: parking.longitude,
+            }}
+          />
+        );
+      })}
+    </MapView>
   );
 };
-export default MapComponent;
 
 const styles = StyleSheet.create({
   map: {
-    height: 200,
+    height: "100%",
     width: "100%",
   },
 });
+
+export default MapComponent;
