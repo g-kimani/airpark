c400ccbc1b8a6bc98f842df03d8ccca9bf69ec2c
Northerner988 <lulu12100@hotmail.co.uk>
2023-06-17 09:46:15 +0100

    create array of cities

---
+++ data/citiesArray.js
@@ -0,0 +1,13 @@
+const citiesArray = [
+  {
+    location: "london",
+  },
+  {
+    location: "bristol",
+  },
+  { location: "leeds" },
+  { location: "essex" },
+  { location: "York" },
+];
+
+export default citiesArray;
