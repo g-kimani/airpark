a49768d8f4f364d09ff96c19317a487d5096a45f
Northerner988 <lulu12100@hotmail.co.uk>
2023-06-19 02:20:46 +0100

    continue setup of individual screen

--- screens/IndividualParking.tsx
+++ screens/IndividualParking.tsx
@@ -1,8 +1,5 @@
 import React, { useState } from "react";
-
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-
-import Calendar from "react-native-calendar-range-picker";
 import {
   Text,
   View,
@@ -10,95 +7,165 @@ import {
   TouchableOpacity,
   Modal,
   Button,
+  Image,
+  SafeAreaView,
 } from "react-native";
+import { Entypo } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
+import tw from "twrnc";
+import moment from "moment";
+import { defaultImage } from "../assets/image_not_found.ts";
+
+import { Calendar } from "react-native-calendars";
 
 type Parking = {
-  // Define the type for the parking object
   parking_id: number;
   latitude: number;
   longitude: number;
   price: number;
-  // Include any other properties of the parking object
-  // that you want to access and display
 };
 
 type IndividualParkingProps = NativeStackScreenProps<any, "IndividualParking">;
 
 const IndividualParking = ({ route }: IndividualParkingProps) => {
-  // Access the parking object from the route parameter
   const { parking } = route.params;
-  const [selected, setSelected] = useState("");
-  const [showCalendar, setShowCalendar] = React.useState(false);
+  const [selectedStartDate, setSelectedStartDate] = useState(null);
+  const [showCalendar, setShowCalendar] = useState(false);
+  const [endDate, setEndDate] = useState(null);
+
   const toggleModal = () => {
     setShowCalendar(!showCalendar);
   };
+
   const handleBooking = () => {};
 
-  // Render the parking information
   return (
-    <>
-      <Text>
-        Individual Parking:
-        {"\n"}
-        Parking ID: {parking.parking_id}
-        {"\n"}
-        Host ID: {parking.host_id}
-        {"\n"}
-        Price: {parking.price}
-        {"\n"}
-        Area: {parking.area}
-        {"\n"}
-        Availability: {parking.is_booked}
-        {/* Render additional information as needed */}
-      </Text>
-
-      <View style={{ margin: 20 }}>
-        <Button title="Select Dates" onPress={toggleModal} />
-        <View style={styles.container}>
-          <Modal visible={showCalendar} animationType="slide">
-            <Calendar
-              startDate={new Date()}
-              endDate
-              scrollToToday
-              scrollable={true}
-              onChange={({ startDate, endDate }) => {
-                if (startDate && endDate) {
-                  setShowCalendar(false);
-                }
-              }}
-            />
-          </Modal>
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Image
+          source={{ uri: parking.picture ? parking.picture : defaultImage }}
+          style={styles.image}
+        />
+        <View style={styles.locationContainer}>
+          <Entypo name="location-pin" size={40} color="red" />
+          <Text style={tw`text-2xl font-bold`}>{parking.area}</Text>
         </View>
+        <Text style={tw`text-xl text-gray-500 my-2 `}>
+          {parking.description}
+        </Text>
+        <View style={styles.priceContainer}>
+          <FontAwesome5 name="pound-sign" size={28} color="#ff8500" />
+          <Text style={styles.price}>{parking.price}</Text>
+        </View>
+
+        <View style={{ margin: 20 }}>
+          <Button title="Select Dates" onPress={toggleModal} />
+          <Modal visible={showCalendar} animationType="slide">
+            <View style={styles.calendarContainer}>
+              <Calendar
+                style={{
+                  marginTop: 20,
+                }}
+                current={moment().format("YYYY-MM-DD")}
+                minDate={moment().format("YYYY-MM-DD")}
+                maxDate={moment().add(2, "months").format("YYYY-MM-DD")}
+                onDayPress={(day) => {
+                  if (selectedStartDate) {
+                    setEndDate(day.dateString);
+                    setShowCalendar(false);
+                  } else {
+                    setSelectedStartDate(day.dateString);
+                  }
+                }}
+                markedDates={{
+                  [selectedStartDate]: {
+                    selected: true,
+                    startingDay: true,
+                    color: "green",
+                  },
+                  [endDate]: { selected: true, color: "green" },
+                }}
+              />
+              {selectedStartDate && endDate && (
+                <TouchableOpacity
+                  onPress={() => {
+                    setShowCalendar(false);
+                  }}
+                  style={styles.closeButton}
+                >
+                  <Text style={styles.closeButtonText}>Confirm</Text>
+                </TouchableOpacity>
+              )}
+            </View>
+          </Modal>
+          {selectedStartDate && endDate && (
+            <Text style={styles.selectedDateText}>
+              {moment(selectedStartDate).format("D")}-
+              {moment(endDate).format("D MMM")}
+            </Text>
+          )}
+        </View>
+        <TouchableOpacity
+          style={tw`rounded-md bg-indigo-600 px-5 py-4 shadow-sm m-6 mx-auto`}
+          onPress={handleBooking}
+        >
+          <Text style={tw`text-sm font-semibold text-white`}>Reserve Spot</Text>
+        </TouchableOpacity>
       </View>
-      <TouchableOpacity style={styles.Button} onPress={handleBooking}>
-        <Text style={styles.ButtonText}>Book the space</Text>
-      </TouchableOpacity>
-    </>
+    </SafeAreaView>
   );
 };
-const styles = StyleSheet.create({
-  ButtonText: {
-    color: "white",
-    fontWeight: "700",
-    fontSize: 16,
-    textAlign: "center",
-  },
-  Button: {
-    padding: 12,
-    backgroundColor: "blue",
-    borderRadius: 10,
-    margin: 5,
-    width: "50%",
-    alignItems: "center",
-  },
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "yellow",
-    padding: 10,
 
+const styles = StyleSheet.create({
+  container: {
+    margin: 20,
+  },
+  image: {
     width: "100%",
+    height: "50%",
+    resizeMode: "cover",
+    borderRadius: 15,
+  },
+  locationContainer: {
+    flexDirection: "row",
+    alignItems: "baseline",
+    marginLeft: -5,
+  },
+  priceContainer: {
+    flexDirection: "row",
+    alignItems: "baseline",
+    fontSize: 30,
+    marginVertical: 10,
+    marginLeft: 5,
+  },
+  price: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginLeft: 6,
+  },
+  calendarContainer: {
+    flex: 1,
+    marginTop: 20,
+  },
+  closeButton: {
+    marginTop: 20,
+    alignSelf: "center",
+    padding: 10,
+    backgroundColor: "lightgray",
+    borderRadius: 5,
+  },
+  closeButtonText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "black",
+  },
+  selectedDateText: {
+    marginTop: 20,
+    fontSize: 16,
+    fontWeight: "bold",
+    textAlign: "center",
+    textDecorationLine: "underline",
   },
 });
+
 export default IndividualParking;
